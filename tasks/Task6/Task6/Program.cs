e202e74c07276aebb9a768e863d655cc1d2d2634
ic16b089 <ic16b089@technikum-wien.at>
2017-04-17 19:31:08 +0200

    only for understandig testing with source code from lesson 6

---
+++ tasks/Task6/Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using System.Collections.Generic; 
+using System.Linq; 
+using System.Text; 
+using System.Threading.Tasks;
+
+
+namespace Task6
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			//PullExample.Run();
+			TasksExample.Run(); 
+
+
+		}
+	}
+}
---
+++ tasks/Task6/Task6/TasksExample.cs
@@ -0,0 +1,81 @@
+﻿//
+// only doing testing on available functions from lesson 6 in this project (to understand how it works)
+// individual testing is done in MyTask6 and in extentions to my original project
+// 17/04/2017 Niedermaier Robert
+//
+
+using System;
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static System.Console;
+
+
+namespace Task6
+{
+public static class TasksExample
+{
+	public static void Run()
+	{
+		var random = new Random();
+
+		var xs = new[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+		var tasks = new List<Task<int>>();
+
+		foreach (var x in xs)
+		{
+			var task = Task.Run(() =>
+			{
+				WriteLine($"computing result for {x}");
+				Task.Delay(TimeSpan.FromSeconds(5.0 + random.Next(10))).Wait();
+				WriteLine($"done computing result for {x}");
+				return x * x;
+			});
+
+			tasks.Add(task);
+		}
+
+		WriteLine("doing something else ...");
+
+		var tasks2 = new List<Task<int>>();
+		foreach (var task in tasks.ToArray())
+		{
+			tasks2.Add(
+				task.ContinueWith(t => { WriteLine($"result is {t.Result}"); return t.Result; })
+			);
+		}
+
+		var cts = new CancellationTokenSource();
+		var primeTask = ComputePrimes(cts.Token);
+
+		ReadLine();
+		cts.Cancel();
+		WriteLine("canceled ComputePrimes");
+
+		ReadLine();
+	}
+
+	public static Task<bool> IsPrime(int x, CancellationToken ct)
+	{
+		return Task.Run(() =>
+		{
+			for (var i = 2; i < x - 1; i++)
+			{
+				ct.ThrowIfCancellationRequested();
+				if (x % i == 0) return false;
+			}
+			return true;
+		}, ct);
+	}
+
+	public static async Task ComputePrimes(CancellationToken ct)
+	{
+		for (var i = 100000000; i < int.MaxValue; i++)
+		{
+			ct.ThrowIfCancellationRequested();
+			if (await IsPrime(i, ct)) WriteLine($"prime number: {i}");
+		}
+	}
+}
+}
